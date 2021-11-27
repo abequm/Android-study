ed9b76ab05351d6bcda454866ad82826d7c378c3
User2 <user2@madskill.ru>
2021-11-27 15:53:49 +0500

    2th view for cafe

--- app/src/main/java/com/kostusenko/labs/Cafe.java
+++ app/src/main/java/com/kostusenko/labs/Cafe.java
@@ -3,10 +3,15 @@ package com.kostusenko.labs;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class Cafe extends Fragment {
 
@@ -21,6 +26,21 @@ public class Cafe extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cafe, container, false);
+        View v =inflater.inflate(R.layout.fragment_cafe, container, false);
+        Button btn= getActivity().findViewById(R.id.lab7_auth);
+        if (btn ==null){
+        }
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        FragmentManager manager = getActivity().getSupportFragmentManager();
+                        String userLogin = ((TextView)(getActivity().findViewById(R.id.lab7_login))).getText().toString();
+                        String userPass = ((TextView)(getActivity().findViewById(R.id.lab7_pass))).getText().toString();
+                        manager.beginTransaction().replace(R.id.frameLayout,new CafeOrderConstructor(userLogin,userPass)).commit();
+                    }
+                }
+        );
+        return v;
     }
 }
